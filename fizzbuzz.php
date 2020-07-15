8dc8641434a52f74a74b831d7cf5935779f7a9d6
max-duval <61746322+max-duval@users.noreply.github.com>
2020-07-15 09:16:37 +0200

    Create fizzbuzz.php

---
+++ fizzbuzz.php
@@ -0,0 +1,17 @@
+<?php
+
+	function fizzBuzz(int $limit = 30) {
+		for ($i = 1; $i < $limit; $i++) {
+			if ($i % 3 == 0 && $i % 5 == 0) {
+				echo 'FizzBuzz<br>';
+			} elseif ($i % 3 == 0) {
+				echo 'Fizz<br>';
+			} elseif ($i % 5 == 0) {
+				echo 'Buzz<br>';
+			} else {
+				echo $i . '<br>';
+			}
+		}
+	}
+
+	fizzBuzz(50);
